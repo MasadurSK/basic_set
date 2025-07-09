dbae9993de4e666ff50394aacb3a44f3c51b5f73
MasadurSK <masadurskrahaman@gmail.com>
2025-07-09 14:18:30 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"
+console.log(3+3)
+console.log("Rakesh")
+let name ="Rakesh"
+let age=18
+let isloggedIn=false
+let state;
+//string =>""
+//null=>standalone value
+//boolean =>true/false/
+//symbol =>unique
+//number => 2 to power 53
+
+
+//object
+console.log(typeof "Rakesh");
+console.log(typeof null)
+console.log(typeof undefined);
+console.log(typeof age);
+
+
