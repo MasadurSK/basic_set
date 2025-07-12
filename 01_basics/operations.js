38443a8730ad0d440485543a9abea3845bf84908
MasadurSK <masadurskrahaman@gmail.com>
2025-07-12 10:46:17 +0000

    Operations

--- 01_basics/operations.js
+++ 01_basics/operations.js
@@ -20,6 +20,9 @@ console.log(+false);
 let num1,num2,num3
 
 num1 = num2 = num3 = 2 + 2;
+console.log(num1);
+console.log(num2);
+console.log(num3);
 let gameCounter=100;
 gameCounter++;
 console.log(gameCounter);
