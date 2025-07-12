cb641493f7503f83428ca876f949550d1841304b
MasadurSK <masadurskrahaman@gmail.com>
2025-07-12 12:04:38 +0000

    datatypes Summary

---
+++ 01_basics/datatypesSum.js
@@ -0,0 +1,27 @@
+const id = Symbol('123')
+const anotherId =  Symbol('123')
+console.log(id === anotherId);
+const bigNumber = 3447867853287n
+const outsideTemp = null;
+
+
+
+const heros = ["shaktiman","naagraj","doga"]
+
+
+//objects
+let myObj = {
+    name:"Rakesh",
+    age:22,
+}
+
+const myFunction = function(){
+  console.log("Hello world");
+}
+
+
+console.log(typeof bigNumber)
+console.log (typeof outsideTemp )
+console.log(typeof myFunction)
+console.log(typeof myObj)
+console.log(typeof id)
