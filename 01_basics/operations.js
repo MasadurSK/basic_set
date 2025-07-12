ec427ce8ac7043616c2a71ca43a6374b6176cfa7
MasadurSK <masadurskrahaman@gmail.com>
2025-07-12 10:42:37 +0000

    Operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,26 @@
+let value =3
+let negValue=-value
+console.log(negValue);
+
+let str1="hello"
+let str2="rakesh"
+let str3 =str1 + str2 
+console.log(str3);
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(2 + 2 + "1");
+console.log(true);
+console.log(+true);
+console.log("");
+console.log(+"");
+console.log(false);
+console.log(+false);
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2;
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
+
